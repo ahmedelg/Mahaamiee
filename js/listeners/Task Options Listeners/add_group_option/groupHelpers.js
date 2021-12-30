69e22013241cd9fd63b1134c0fd9a1955893bdf8
Ahmed Al-Gaied <46943991+ahmedelg@users.noreply.github.com>
2021-12-30 05:48:27 +0200

    Add files via upload

---
+++ js/listeners/Task Options Listeners/add_group_option/groupHelpers.js
@@ -0,0 +1,34 @@
+const GroupHelpers = (() => { 
+  // # Get `task`
+  // const getTaskFOption = p => p.parentElement.parentElement.parentElement.parentElement;
+  
+  // const createNewGroup = () => {
+  //   let groupDtls = {};
+  //   if (isThereCreatedGroups()) { 
+  //     // ? There are created groups
+  //     groupDtls.number = LST_CRT_GR_NUM();
+  //     // console.log(groupDtls)
+  //     return new Group(groupDtls);
+  //   } else { 
+  //     // ? There are no-created groups
+  //     return new Group(groupDtls);
+  //   };
+  // };
+  
+  // const addNewGroup = (gr) => Created_Groups.push(gr);
+  
+  // const LST_CRT_GR_NUM = () => getLastCreatedGroup().NEXT_GROUP_NUMBER();
+  
+  // const getLastCreatedGroup = () => Created_Groups[Created_Groups.length - 1];
+  
+  
+  // const isThereCreatedGroups = () => Created_Groups.length ? true : false;
+  
+  return {
+    // getTaskFOption: getTaskFOption,
+    // CREATED_NEW_GROUP: createNewGroup,
+    // addNewGroup: addNewGroup
+    
+  };
+})();
+// End!
