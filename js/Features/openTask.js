414a140a7fe32deccf19bd88054940680361679a
ahmedelg <ahmedelgiedfuture@gmail.com>
2022-01-24 08:18:15 +0200

    Start Dev openTask Feature

---
+++ js/Features/openTask.js
@@ -0,0 +1,7 @@
+/* #############################################################
+                     TASK OPEN OPTION
+              ABOUT USERS NEED TO  OPEN TASK
+      & DISPLAY DETAILS F TASK - HANDLE DETAILS F TASK
+   #############################################################  */
+"use strict";
+const openTask = () => {};
